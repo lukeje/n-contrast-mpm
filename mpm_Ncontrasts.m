7f2ba45be438e636b00aa5be814c7ef3c795d9b1
ledwards <ledwards@cbs.mpg.de>
2025-01-28 14:56:43 +0100

    Bugfixes and add progress bar

--- mpm_Ncontrasts.m
+++ mpm_Ncontrasts.m
@@ -19,54 +19,70 @@ Vref = spm_vol(char(contrasts{1}(1)));
 
 %% Get files
 % Extract metadata
-for c = 1:length(contrasts)
+for c = length(contrasts):-1:1 % allocate backwards to get rid of matlab warnings about needing preallocation
     currentFiles = contrasts{c}(:);
     nEchoes = length(currentFiles);
-    weightedData(c).TE = zeros([1,nEchoes]); %#ok<AGROW>
-    weightedData(c).TR = zeros([1,nEchoes]); %#ok<AGROW>
-    weightedData(c).fa = zeros([1,nEchoes]); %#ok<AGROW>
+    weightedData(c).TE = zeros([1,nEchoes]);
+    weightedData(c).TR = zeros([1,nEchoes]);
+    weightedData(c).fa = zeros([1,nEchoes]);
     for echo = 1:nEchoes
-        imageFile = fullfile(currentFiles(echo).folder,currentFiles(echo).name);
+        imageFile = currentFiles(echo);
         fid = fopen(strrep(imageFile,"nii","json"), 'r');
         bidsJson = jsondecode(fscanf(fid,"%s"));
-        bidsJson = bidsJson.acqpar;
-        fclose(fid);
-        weightedData(c).TE(echo) = bidsJson.EchoTime*1e-3;
-        weightedData(c).TR(echo) = bidsJson.RepetitionTime*1e-3;
+        if isfield(bidsJson,'acqpar')
+            bidsJson = bidsJson.acqpar;
+            weightedData(c).TE(echo) = bidsJson.EchoTime*1e-3;
+            weightedData(c).TR(echo) = bidsJson.RepetitionTime*1e-3;
+        else
+            weightedData(c).TE(echo) = bidsJson.EchoTime;
+            weightedData(c).TR(echo) = bidsJson.RepetitionTimeExcitation;
+        end
         weightedData(c).fa(echo) = deg2rad(bidsJson.FlipAngle);
+        fclose(fid);
+        
         assert(weightedData(c).TR(echo)==weightedData(c).TR(1), "TR must match within a contrast!")
         assert(weightedData(c).fa(echo)==weightedData(c).fa(1), "Flip angle must match within a contrast!")
     end
+    weightedData(c).TR = weightedData(c).TR(1);
+    weightedData(c).fa = weightedData(c).fa(1);
 end
 
 %% Fit R2*
 R2star = nan(Vref.dim);
+%DeltaR2star = nan(Vref.dim);
 extrapolated = cell(length(contrasts),1);
 for c = 1:length(contrasts)
     extrapolated{c} = nan(Vref.dim);
 end
+spm_progress_bar('Init',Vref.dim(3),'R2* fit');
 for z=1:Vref.dim(3) % process data by slice
     for c = 1:length(contrasts)
         currentFiles = char(contrasts{c}(:)); % SPM requires char array
-        nEchoes = length(currentFiles);
+        nEchoes = size(currentFiles,1);
         weightedData(c).data = nan([Vref.dim(1:2),nEchoes]);
         for echo = 1:nEchoes
-            imageFile = fullfile(currentFiles(echo).folder,currentFiles(echo).name);
+            imageFile = currentFiles(echo,:);
             V=spm_vol(imageFile);
             weightedData(c).data(:,:,echo) = spm_slice_vol(V,spm_matrix([0 0 z]),Vref.dim(1:2),0);
         end
         weightedData(c).data(weightedData(c).data<eps) = eps;
     end
     [R2star(:,:,z),extrapolatedz] = hmri_calc_R2s(weightedData,"WLS1");
+    %[R2star(:,:,z),extrapolatedz,DeltaR2star(:,:,z)] = hmri_calc_R2s(weightedData,"WLS1","linear");
     for c = 1:length(contrasts)
         extrapolated{c}(:,:,z) = extrapolatedz{c};
     end
+    spm_progress_bar('Set',z);
 end
+spm_progress_bar('Clear');
 
 Vout = Vref;
 Vout.dt(1) = spm_type('float32');
 Vout.fname = char(fullfile(outdir,"R2star.nii"));
 spm_write_vol(Vout,R2star);
+
+%Vout.fname = char(fullfile(outdir,"DeltaR2star.nii"));
+%spm_write_vol(Vout,DeltaR2star);
 
 TEzerofile = cell(length(contrasts),1);
 for c = 1:length(contrasts)
@@ -88,16 +104,20 @@ clear weightedData
 %%
 T1 = nan(Vref.dim);
 A  = nan(Vref.dim);
+spm_progress_bar('Init',Vref.dim(3),'T1 fit');
 for z = 1:Vref.dim(3) % process data by slice
     B1 = hmri_read_vols(spm_vol(char(b1map)),Vref,z,3)*0.01;
     for c = 1:length(contrasts)
         dat0(c).data = hmri_read_vols(spm_vol(TEzerofile{c}),Vref,z,3);
     end
 
-    mask = t1w0.data>threshold;
+    mask = dat0(1).data>threshold;
 
     [A(:,:,z),T1(:,:,z)]=weighted2AT1(dat0,B1,mask);
+
+    spm_progress_bar('Set',z);
 end
+spm_progress_bar('Clear');
 
 VT1 = Vout;
 VT1.fname = char(fullfile(outdir,"T1map.nii"));
